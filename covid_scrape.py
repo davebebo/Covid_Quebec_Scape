bc5d3b412cd8e3ca00fb478a2e1e45c0111fd910
Dave <bourbeau.dave@gmail.com>
2020-03-18 18:29:46 -0400

    Doing first commit of code to local repo

---
+++ covid_scrape.py
@@ -0,0 +1,47 @@
+##############################################################################################
+#
+#  Author  : Dave Bourbeau
+#  Program : covid_scrape.py
+#  Python  : Python3
+#  Description : This program scrapes the Quebec government Website for the current case
+#                of covid cases in each region display and stores it in a text file daily
+##############################################################################################
+import requests
+import datetime
+from bs4 import BeautifulSoup
+
+# Global local variables
+first_table_elem = 0
+i=0
+table = []
+
+#Create a Request object of the website
+URL = 'https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/'
+page = requests.get(URL)
+
+#Get current date, and create text file with date appended
+today = datetime.datetime.now()
+filename = "Covid_data_QC_" + today.strftime("%Y") + "_" + today.strftime("%m") + "_" + today.strftime("%d")
+file = open(filename, "w+")
+
+#Create beautiful soup object from the page request for easy filtering of HTML
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#Gather all text in the Table field of the website and enter into a list
+results = soup.findAll("td")
+for p in results:
+    table.append(p.text)
+
+table_len = len(table)
+
+#Filter trough elements of table and output everything after Mauricie, formated to text file
+while i < table_len - 1:
+	if "Mauricie" in table[i]:
+		first_table_elem = 1
+	if first_table_elem == 1:
+		print("[",table[i],"] ","[", table[i+1],"] ")
+		text = "[" +table[i]+ "] " + "[" + table[i+1] + "] " + "\n"
+		file.write(text)
+		i = i+2
+	else:
+		i = i+1
---
+++ covid_scrape.sh
@@ -0,0 +1,4 @@
+#/usr/local/bin/zsh
+
+cd /Users/davebourbeau/Documents/covid
+python3 covid_scrape.py
